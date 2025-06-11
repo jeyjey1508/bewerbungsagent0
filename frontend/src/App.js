f80caf370ffc04c30387094aed3452683dd3c770
jeyjey1508 <jeremyhayen2007@yahoo.com>
2025-06-11 22:31:35 +0200

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import axios from "axios";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-const API = ${BACKEND_URL}/api;
+const API = `${BACKEND_URL}/api`;
 
 function App() {
   const [formData, setFormData] = useState({
