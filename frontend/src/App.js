e0a3945c28175ca58d9168ea72848ea0cb8e8306
jeyjey1508 <jeremyhayen2007@yahoo.com>
2025-06-18 15:25:00 +0200

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,6 @@ const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const API = `${BACKEND_URL}/api`;
 
 function App() {
-  const [lizenzKey, setLizenzKey] = useState("");
-  const [isVerified, setIsVerified] = useState(false);
   const [formData, setFormData] = useState({
     personal: {
       vorname: "",
